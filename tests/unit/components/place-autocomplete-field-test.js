c8e382afa344afb1c2c9dbdef3fafde7258d5398
dmuneras <dmunera119@gmail.com>
2015-10-13 02:56:41 -0500

    Adding unit test for component

---
+++ tests/unit/components/place-autocomplete-field-test.js
@@ -0,0 +1,41 @@
+/* jshint expr:true */
+import { expect } from 'chai';
+import {
+  describeComponent,
+  it
+} from 'ember-mocha';
+
+describeComponent(
+  'place-autocomplete-field',
+  'PlaceAutoCompleteComponent',
+  {
+    // specify the other units that are required for this test
+    needs: ['model:fake-model']
+  },
+
+  function() {
+    it('renders', function() {
+      var component = this.subject();
+      expect(component._state).to.equal('preRender');
+
+      // renders the component on the page
+      this.render();
+      expect(component._state).to.equal('inDOM');
+    });
+
+    it("accetps 'label' option", function() {
+      var component = this.subject();
+      component.set('label','address fake label');
+      this.render();
+      expect(component.get('label')).to.equal('address fake label');
+    });
+
+    it("accetps 'value' option", function() {
+      var component = this.subject();
+      var fakeModel = { address: 'fake address'};
+      component.set('value',fakeModel.address);
+      this.render();
+      expect(component.get('value')).to.equal('fake address');
+    });
+  }
+);
