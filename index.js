6c769fe72a23cd88da6a49de6672591a153bf9e4
Steve Szczecina <steveszc@gmail.com>
2016-06-27 14:49:20 -0600

    Add config for client ID for premium g maps users

--- index.js
+++ index.js
@@ -10,10 +10,13 @@ module.exports = {
          placeAutocompleteConfig = config['place-autocomplete'] || {},
          params = [],
          exclude = placeAutocompleteConfig.exclude,
+         client = placeAutocompleteConfig.client,
          key = placeAutocompleteConfig.key;
       if (!exclude) {
         if (key)
           params.push('key=' + encodeURIComponent(key));
+        if (client)
+          params.push('client=' + encodeURIComponent(client) + '&v=3.24');
         src += '?' + params.join('&') + "&libraries=places";
         content = '<script type="text/javascript" src="' + src + '"></script>';
       }
