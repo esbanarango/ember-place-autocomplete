cb35cf47424dcacfa848d13ba882bb0ff37abcc1
dmuneras <ds@linkfire.com>
2020-05-25 17:07:51 +0200

    refactor: Get code snippet for test example directly from source code

--- tests/integration/components/place-autocomplete-field-test.js
+++ tests/integration/components/place-autocomplete-field-test.js
@@ -1,3 +1,5 @@
+// BEGIN-SNIPPET tests-example.js
+
 import EmberObject from '@ember/object';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
@@ -10,30 +12,22 @@ module('Integration | place-autocomplete-field', function(hooks) {
 
   test('renders passed attributes properly', async function(assert) {
     const fakeModel = EmberObject.create({ address: 'fake address'});
-    const passedProperties = EmberObject.create({
-      placeholder: 'fake placeholder',
-      name: 'fake-name',
-      inputClass: 'fake-input-class',
-      fakeModel,
-      setValueWithProperty: null
-    });
-
-    this.set('passedProperties', passedProperties);
 
     await render(hbs`
       {{place-autocomplete-field
-        placeholder=passedProperties.placeholder
-        name=passedProperties.name
-        inputClass=passedProperties.inputClass
+        placeholder='fake placeholder'
+        name='fake-name'
+        inputClass='fake-input-class'
         value=fakeModel.address
-        setValueWithProperty=passedProperties.setValueWithProperty
         data-independiente-medellin='what is that'
       }}
     `);
 
-    assert.strictEqual(find('input').getAttribute('placeholder').trim(), 'fake placeholder', 'Renders passed input placeholder');
-    assert.strictEqual(find('input').getAttribute('name').trim(), 'fake-name', 'Renders passed input name');
-    assert.strictEqual(find('input').classList.contains('fake-input-class'), true, 'Adds passed inputClass to input classes');
+    const inputElement = find('input');
+
+    assert.strictEqual(inputElement.getAttribute('placeholder').trim(), 'fake placeholder', 'Renders passed input placeholder');
+    assert.strictEqual(inputElement.getAttribute('name').trim(), 'fake-name', 'Renders passed input name');
+    assert.strictEqual(inputElement.classList.contains('fake-input-class'), true, 'Adds passed inputClass to input classes');
     assert.strictEqual(fakeModel.address, 'fake address', "unset 'setValueWithProperty' option does not affect entered address");
     assert.ok(find('input[data-independiente-medellin]'));
   });
@@ -64,3 +58,4 @@ module('Integration | place-autocomplete-field', function(hooks) {
     );
   });
 });
+// END-SNIPPET
